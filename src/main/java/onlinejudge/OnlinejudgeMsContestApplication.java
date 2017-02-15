04b883ed37d4ccaf4e0bcd9644bfc6af43e04613
Hoang Nhuoc Quy <hoangnhuocquy0@gmail.com>
2017-02-16 00:35:44 +0700

    Reconfig: add author for Feign and check token access.

--- src/main/java/onlinejudge/OnlinejudgeMsContestApplication.java
+++ src/main/java/onlinejudge/OnlinejudgeMsContestApplication.java
@@ -1,12 +1,31 @@
 package onlinejudge;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.security.oauth2.client.feign.OAuth2FeignRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import feign.RequestInterceptor;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,7 +34,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @EnableOAuth2Client
 @EnableFeignClients
 public class OnlinejudgeMsContestApplication {
-
+	@Autowired
+	private Environment env;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(OnlinejudgeMsContestApplication.class, args);
 	}
